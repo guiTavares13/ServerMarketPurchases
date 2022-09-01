251791e435b886b6b756350fdbb0f4d3a0913242
guiTavares13 <guiftt035@outlook.com>
2022-09-01 11:23:21 -0300

    Project created

---
+++ src/main/java/com/developer/mercadoServer/MercadoServerApplication.java
@@ -0,0 +1,13 @@
+package com.developer.mercadoServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/developer/mercadoServer/MercadoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.mercadoServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
